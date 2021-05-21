9da9448777b87dbe86fbdcd5e6e2b52e7fae0e5d
Rimsan <rimsnet@gmail.com>
2021-05-21 10:16:00 +0800

    Advanced Express JS REST API [#4] Register Endpoint | Building REST API Node JS

--- jwt-authentication/src/index.ts
+++ jwt-authentication/src/index.ts
@@ -1,13 +1,61 @@
 import "reflect-metadata";
-import {createConnection} from "typeorm";
-import {User} from "./entity/User";
+import { createConnection } from "typeorm";
+import { User } from "./entity/User";
 
 import * as express from 'express';
+import { Request, Response } from 'express';
+import { Database } from "./database";
+import { RegisterDTO } from "./dto/request/register.dto";
 
 const app = express();
 
+app.use(express.json());
 
-app.listen(4000, ()=>{ console.log('Listening on port', 4000) });
+Database.initialize();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello there");
+});
+
+app.post("/register", (req: Request, res: Response) => {
+
+  try {
+
+    const body: RegisterDTO = req.body;
+
+    //validate the body
+    if (body.password !== body.repeatPassword)
+      throw new Error('Repeat password does not match')
+
+    //validate if the email is already being used
+    if(Database.userRepository.findOne({email:body.email})){
+      throw new Error('E-mail is already being used')
+    }
+
+    //store the user
+    const user = new User();
+    user.username = body.username;
+    user.email = body.email;
+
+
+    res.json({
+      token: "dummy-token",
+      refreshToken: "dummy-refreshToken",
+      user: {
+        id: 1,
+        username: "dummy-username"
+      }
+    })
+
+  } catch (error) {
+    res.status(500).json({
+      messsage: error.message
+    });
+  }
+});
+
+
+app.listen(4000, () => { console.log('Listening on PORT', 4000) });
 
 createConnection().then(async connection => {
 
