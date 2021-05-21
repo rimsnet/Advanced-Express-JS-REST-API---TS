df550068b1d105ee71bd9882124426339462755b
Rimsan <rimsnet@gmail.com>
2021-05-21 09:35:37 +0800

    initial

---
+++ jwt-authentication/src/index.ts
@@ -0,0 +1,31 @@
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import {User} from "./entity/User";
+
+import * as express from 'express';
+
+const app = express();
+
+
+app.listen(4000, ()=>{ console.log('Listening on port', 4000) });
+
+createConnection().then(async connection => {
+
+  /*  
+    console.log("Inserting a new user into the database...");
+    const user = new User();
+    user.firstName = "Timber";
+    user.lastName = "Saw";
+    user.age = 25;
+    await connection.manager.save(user);
+    console.log("Saved a new user with id: " + user.id);
+
+    console.log("Loading users from the database...");
+    const users = await connection.manager.find(User);
+    console.log("Loaded users: ", users);
+
+    console.log("Here you can setup and run express/koa/any other framework."); 
+    
+  */
+
+}).catch(error => console.log(error));
